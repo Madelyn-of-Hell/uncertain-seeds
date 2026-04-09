928274c2db92080a25a06f2859b0ec4e867e28fe
Maddie Bell-Leemen <maddie_bl@protonmail.com>
2026-04-09 11:53:11 +1000

    Implemented basic state shifting system

--- Player/player.gd
+++ Player/player.gd
@@ -2,17 +2,20 @@ extends CharacterBody2D
 static var horizontal_movement = 1500
 static var jump_strength = 30000
 static var top_speed = 225
+
 var state:State = State.COLLECTED
 enum State {
 	COLLECTED,
 	PRIMAL
 }
-@onready var floor_grabber = $FloorGrabber
+@onready var floor_grabber = $"FloorGrabber"
 func _physics_process(delta: float) -> void:
 	
 	var collider = floor_grabber.get_collider()
-	
+	print(collider)
 	var friction = collider.friction if collider != null else 1
+	
+	# Handle Input
 	if Input.is_action_pressed("Left") and velocity.x > -top_speed:
 		add_velocity_x(-horizontal_movement, delta)
 	if Input.is_action_pressed("Right") and velocity.x < top_speed:
@@ -38,7 +41,16 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("Change Fuel Cell"):
 		print("Change Fuel Cell")
 	if Input.is_action_just_pressed("Change States"):
-		print("Change States")
+		match state:
+			State.COLLECTED:
+				print("Switch to Primal")
+				state = State.PRIMAL
+				$Animator.play("To Primal")
+			State.PRIMAL:
+				print("Switch to Collected")
+				state = State.COLLECTED
+				$Animator.play("To Collected")
+
 	if Input.is_action_just_pressed("Attack"):
 		print("Attack")
 	if Input.is_action_just_pressed("Alt Attack"):
